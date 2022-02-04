17cab8ed1f6568aab6d741a9d4e55b0fb8dd62fe
Chirag Rajpal <chiragbrajpal@gmail.com>
2022-02-04 12:58:37 -0800

    Create ArrayListPractise.java

---
+++ ArrayListPractise.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Int[] arr=new arr;
+        int[] arr=new int[10];
+        // arr={1,2,3};
+        int[] arr1={1,2,3};
+        arr[0]=1;
+        for(int x : arr1){
+            // System.out.print(x);
+        }
+        
+        
+        ArrayList arrList=new ArrayList<>();
+        arrList.add("Apple");
+        arrList.add("Oranges");
+        System.out.println(arrList);
+        
+        arrList.remove("Oranges");
+        arrList.contains("Apple");
+        arrList.clear();
+        
+        
+        //remove(""), add(""), clear()
+    }
+}
