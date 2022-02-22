7191076987b3ce89857d709d2e1e40189d701e7e
Chirag Rajpal <chiragbrajpal@gmail.com>
2022-02-22 14:14:10 -0800

    Update HMap.java

--- HMap.java
+++ HMap.java
@@ -17,6 +17,11 @@ public class HMap {
 		System.out.println("Map with integer: "+map.get(1));
 		
 		System.out.println(Hmap.get("ABC"));
+		
+		HashMap<String,<List>> map1=new HashMap<>();
+		map1.put("abc",[]);
+		map1.get("abc").add("Hello");
+		
 		System.out.println(Hmap.containsKey("ABC"));	//O(1) time
 		System.out.println(Hmap.containsValue("PASSKEY7867678"));		//O(n) time
 		Hmap.size();
